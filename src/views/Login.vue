3cf8d719919effca57071a05fd1a7c436acfaa5b
scool <zhangfycz@126.com>
2020-08-09 15:32:03 +0800

    first

---
+++ src/views/Login.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <el-form class="" :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
+            <h3 class="loginTitle">系统登录</h3>
+            <el-form-item prop="username">
+                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input type="text" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-checkbox v-model="checked" class="loginRemember"></el-checkbox>
+            <el-button type="primary" style="width:100%;" @click = "submitLogin">登录</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+            return{
+                rules:{
+                    username:[{required:true,message:'请输入用户名',trigger:'blur'}],
+                    password:[{required:true,message:'请输入密码',trigger:'blur'}]
+                },
+                checked:true,
+                loginForm:{
+                    username:'admin',
+                    password:''
+                }
+            }
+        },
+        methods:{
+            submitLogin(){
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        this.$message.error('错了哦，这是一条错误消息');
+                        return false;
+                    }
+                });
+            }
+        }
+    }
+</script>
+<style>
+    .loginContainer{
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 180px auto;
+        width: 350px;
+        padding: 15px 35px 15px 35px;
+        background: #fff;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+    .loginTitle{
+        margin: 15px auto 40px auto;
+        text-align: center;
+        color: #505458;
+    }
+    .loginRemember{
+        text-align: left;
+        margin: 0px 0px 15px 0px;
+    }
+</style>
+
+<style scoped>
+
+</style>
